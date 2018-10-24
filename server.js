4ee42acbdf3249a46687a2d651e109659401c8fd
Teik Yee Lim <limteikyee@gmail.com>
2018-10-24 14:46:08 +0800

    add env fiel

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+require('dotenv').config();
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -12,25 +13,35 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: process.env.DB_PASS,
+    database: 'smart-brain'
   }
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(database.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res) => {
+  res.send(database.users);
+});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log('app is running on port 3000');
-})
+});
